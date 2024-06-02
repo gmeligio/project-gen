969a848fcdf141bb53d78d7403fe5ad7c5ec813c
Eligio Mariño <22875166+gmeligio@users.noreply.github.com>
2024-06-02 10:57:54 +0200

    chore: remove unused code

--- src/patch/JsiiProjectPatch.ts
+++ src/patch/JsiiProjectPatch.ts
@@ -130,25 +130,8 @@ export class JsiiProjectPatch extends JsiiProject {
     // Override build.yml
     const buildWorkflow = this.github!.tryFindWorkflow('build')!;
 
-    // buildWorkflow?.file?.addOverride('jobs.package-js.env.CI', 'true');
-
-    // Move pnpm/action-setup before actions/setup-node to fix error "No pnpm version is specified."
-    // buildWorkflow?.file?.addOverride('jobs.package-js.steps.0.name', 'Setup pnpm');
-    // buildWorkflow?.file?.addOverride('jobs.package-js.steps.0.uses', this.github?.actions.get('pnpm/action-setup'));
-    // buildWorkflow?.file?.addDeletionOverride('jobs.package-js.steps.0.with');
-
-    // Move actions/setup-node after pnpm/action-setup to fix error "No pnpm version is specified."
-    // buildWorkflow?.file?.addDeletionOverride('jobs.package-js.steps.3.name');
-    // buildWorkflow?.file?.addOverride('jobs.package-js.steps.3.uses', this.github?.actions.get('actions/setup-node'));
-    // buildWorkflow?.file?.addOverride('jobs.package-js.steps.3.with.node-version', 'lts/*');
-
     // Delete `with: <pnpm-version>` and unnecessary fields in `build` job after moving actions in steps
     buildWorkflow?.file?.addDeletionOverride('jobs.build.steps.1.with');
-
-    // Delete `with: <pnpm-version>` and unnecessary fields in `package-js` job after moving actions in steps
-    // buildWorkflow?.file?.addDeletionOverride('jobs.package-js.steps.0.with');
-    // buildWorkflow?.file?.addDeletionOverride('jobs.package-js.steps.3.name');
-    // buildWorkflow?.file?.addDeletionOverride('jobs.package-js.steps.3.with.version');
   }
 
   /**
