840e42a3e1bc00b57f4b3241c57cbac222f25b17
Eligio Mariño <22875166+gmeligio@users.noreply.github.com>
2026-03-14 18:19:38 +0100

    fix: eslint format

--- test/sriToHexDigest.test.ts
+++ test/sriToHexDigest.test.ts
@@ -3,7 +3,8 @@ import { sriToHexDigest } from '../src/patch/sriToHexDigest';
 describe('sriToHexDigest', () => {
   test('converts SRI sha512 digest to corepack hex format', () => {
     const sri = 'sha512-QYcvA3rSL3NI47Heu69+hnz9RI8nJtnPdMCPGVB8MdLI56EVJbmD/rwt9kC1Q43uYCPrsfhO1DzC1lTSvDJiZA==';
-    const expected = 'sha512.41872f037ad22f7348e3b1debbaf7e867cfd448f2726d9cf74c08f19507c31d2c8e7a11525b983febc2df640b5438dee6023ebb1f84ed43cc2d654d2bc326264';
+    const expected =
+      'sha512.41872f037ad22f7348e3b1debbaf7e867cfd448f2726d9cf74c08f19507c31d2c8e7a11525b983febc2df640b5438dee6023ebb1f84ed43cc2d654d2bc326264';
     expect(sriToHexDigest(sri)).toBe(expected);
   });
 
